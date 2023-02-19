c41a8058960de03b9388ce698e07d4507af26322
Young Il <youngilkim2000@gmail.com>
2023-02-19 20:29:57 +0900

    adding more templates from 2020 p1

---
+++ 2020_p1/APCalendar.java
@@ -0,0 +1,24 @@
+
+public class APCalendar {
+    public APCalendar() {
+
+    }
+
+    private static boolean isLeapYear(int year) {
+
+    }
+
+    public static int numberOfLeapYears(int year1, int year2) {
+
+    }
+
+    private static int firstDayOfYear(int year) {
+
+    }
+
+    private static int dayOfYear(int month, int day, int year){\
+    }
+
+    public static int dayOfWeek(int month, int day, int year) {
+    }
+}
